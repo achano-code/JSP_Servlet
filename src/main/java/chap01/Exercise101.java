d0f450e0920acc45d745341323b78c517b347841
achano <hyuga.workplace@gmail.com>
2025-07-21 13:05:03 +0900

    jsp

--- src/main/java/chap01/Exercise101.java
+++ src/main/java/chap01/Exercise101.java
@@ -10,23 +10,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Sample101
+ * Servlet implementation class Exercise101
  */
-@WebServlet("/hello")
-public class Sample101 extends HttpServlet {
+@WebServlet("/Exercise101")
+public class Exercise101 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
 		out.println("<html>");
-		out.println("<head></head>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("<title>Exercise101</title>");
+		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Hello Servlet!!!!</h1>");
+		out.println("<p>This is my first Servlet!</p>");
 		out.println("</body>");
-		out.println("</html>");
+		out.println("</html>");	
 	}
 
 }
---
+++ src/main/webapp/chap02/includetarget.jsp
@@ -0,0 +1,1 @@
+<p>Include Target</p>
